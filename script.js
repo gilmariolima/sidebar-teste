8cedd601ed9af4e8d8066d7135ef79b77e232027
Gilmario <gilmario12.2018@gmail.com>
2025-05-17 21:45:59 -0300

    consertando paddings

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnMenu = document.getElementById("btn-menu")
 const sidebar = document.getElementById("sidebar")
-const span = document.querySelectorAll("span")
+const textoSide = document.querySelectorAll(".textoSide")
 const button = document.querySelectorAll("button")
 const principal = document.getElementById("principal")
 const btAtivo = document.querySelectorAll(".btn-ativo")
@@ -12,8 +12,8 @@ btnMenu.addEventListener("click", function(){
         sidebar.classList.remove("ativa")
         sidebar.classList.add("desativa")
 
-        principal.style.marginLeft = "80px"
-        span.forEach(item=>{item.style.display = "none"})
+        principal.style.marginLeft = "100px"
+        textoSide.forEach(item=>{item.style.display = "none"})
         button.forEach(item=>{item.style.justifyContent = "center"})
         
     }else if(sidebar.classList.contains("desativa")){
@@ -22,7 +22,7 @@ btnMenu.addEventListener("click", function(){
 
         principal.style.marginLeft = "250px"
         button.forEach(item => item.style.justifyContent = "flex-start");
-        setTimeout(function(){ span.forEach(item => item.style.display = "flex");}, 400);
+        setTimeout(function(){ textoSide.forEach(item => item.style.display = "flex");}, 200);
     }
 })
 
